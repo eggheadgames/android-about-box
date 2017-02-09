690d7547f1537bd823de548d5f5eb6e5a1f90e7c
Alex Dibrivnyi <sashok.dib@gmail.com>
2017-02-09 21:30:26 +0200

    Added footer to email body (Close #5)

--- library/src/main/java/com/eggheadgames/aboutbox/share/EmailUtil.java
+++ library/src/main/java/com/eggheadgames/aboutbox/share/EmailUtil.java
@@ -3,8 +3,12 @@ package com.eggheadgames.aboutbox.share;
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
+import android.text.TextUtils;
 
 import com.eggheadgames.aboutbox.AboutConfig;
+
+import org.w3c.dom.Text;
 
 public final class EmailUtil {
 
@@ -29,17 +33,27 @@ public final class EmailUtil {
             default:
                 emailSubject = config.emailSubject;
         }
+        String emailBody = config.emailBody;
+        if (TextUtils.isEmpty(emailBody)) {
+            String deviceInfo = "";
+            deviceInfo += "\n App version: " + config.version;
+            deviceInfo += "\n Android version: " + Build.VERSION.RELEASE + " (" + android.os.Build.VERSION.SDK_INT + ")";
+            deviceInfo += "\n OS version: " + System.getProperty("os.version") + " (" + android.os.Build.VERSION.INCREMENTAL + ")";
+            deviceInfo += "\n Device: " + android.os.Build.MODEL + " (" + android.os.Build.PRODUCT + ")";
+
+            emailBody = "Please type your question here: \n\n\n\n\n"
+                    + "---------------------------" + deviceInfo;
+        }
 
         try {
             Intent emailIntent = new Intent(Intent.ACTION_SENDTO, mailto);
             emailIntent.putExtra(Intent.EXTRA_SUBJECT, emailSubject);
-            emailIntent.putExtra(Intent.EXTRA_TEXT, config.emailBody);
+            emailIntent.putExtra(Intent.EXTRA_TEXT, emailBody);
             activity.startActivity(Intent.createChooser(emailIntent, "Send email..."));
         } catch (Exception e) {
             if (config.analytics != null) {
                 config.analytics.logException(e, false);
             }
         }
-
     }
 }
